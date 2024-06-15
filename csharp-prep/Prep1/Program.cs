715d5cb06b507606e749b7a116e658465e6a04d5
Ebenezer Gasonoo <iamknightrae@gmail.com>
2024-06-15 01:51:46 +0000

    fixed the spellings and added comments in the code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,13 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Ask for Name
         Console.Write("What is your  First Name?  ");
         string  FirstName = Console.ReadLine();
 
         Console.Write("What is your Last Name?  ");
         string LastName = Console.ReadLine();
 
-        Console.Write($"Your Name is {LastName}, {FirstName} {LastName}. ");
+        Console.Write($"Your name is {LastName}, {FirstName} {LastName}. ");
 
     }
 }
