250fd35a55bdad7292c2720565164814810fda91
Ebenezer Gasonoo <iamknightrae@gmail.com>
2024-06-15 01:44:22 +0000

    C# Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your  First Name?  ");
+        string  FirstName = Console.ReadLine();
+
+        Console.Write("What is your Last Name?  ");
+        string LastName = Console.ReadLine();
+
+        Console.Write($"Your Name is {LastName}, {FirstName} {LastName}. ");
+
     }
 }
